4f4be8ba308f2e89a520f17c4b8bc688b21f1aaa
Blake Lee <blakelee@Blakes-MacBook-Pro.local>
2020-08-19 19:12:59 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+import parser from 'body-parser';
+
+const app = express();
+const PORT = 6969;
+
+app.use(parser.json());
+app.use(parser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, '../public/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/dist/index.html'))
+});
+
+app.listen(PORT, () => console.log(`Connected to PORT: ${PORT}`))
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve('./src'),
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env', '@babel/react']
+        }
+      },
+      {
+        test: [/\.css$/, /\.scss$/],
+        loader: ['style-loader', 'css-loader', 'sass-loader'],
+        
+      },
+      {
+        test: /\.(gif|png|jpe?g|pdf|mp4|ttf)$/i,
+        loader: 'file-loader',
+        options: {
+          esModule: false
+        }
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ]
+  },
+  plugins: [
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  watchOptions: {
+    poll: true,
+    ignored: /node_modules/
+  },
+  mode: 'development',
+  optimization: {
+    minimize: true
+  },
+}
