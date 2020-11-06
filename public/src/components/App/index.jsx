1b12a60ad4656219d7fef724a962f61116f384ce
Blake Lee <blakelee@Blakes-MacBook-Pro.local>
2020-11-05 17:14:22 -0800

    [add] navbar

--- public/src/components/App/index.jsx
+++ public/src/components/App/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Navigation from '../Navigation'
 import Home from '../Home';
 
 const App = () => {
   return(
     <div>
+      <Navigation />
       <Home />
     </div>
   )
---
+++ public/src/components/Navigation/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './navigation.scss';
+
+const Navigation = () => {
+
+  return (
+    <div className="navigation__container">
+      
+    </div>
+  )
+}
+
+export default Navigation;
