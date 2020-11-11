5a5328358977a60a25b078d6bc0ab050a86a7b6b
blake-swl <seulwoolee93@gmail.com>
2020-11-10 16:21:18 -0800

    [edit] photos

--- public/src/components/Photography/index.jsx
+++ public/src/components/Photography/index.jsx
@@ -23,11 +23,13 @@ const Photography = () => {
 
   return (
     <div className="photography__container">
-      <PhotoNav />
+      {/* <PhotoNav /> */}
       <div className="photography__banner">
         <h1>Photography</h1>
       </div>
       <div className="photography__body">
+        <h2>Selected Work</h2>
+        <p>Based in Los Angeles, CA, I am a commercial product photographer with a passion for landscape and street photography. </p>
         <div className="row">
           <div className="col">
             <img src={grimes} alt=""/>
