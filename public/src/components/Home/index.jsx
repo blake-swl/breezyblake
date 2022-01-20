2352713b3d6e42303dc01d238dcb11f2200914e0
blake-swl <seulwoolee93@gmail.com>
2022-01-19 21:19:11 -0800

    [fix] works

--- public/src/components/Home/index.jsx
+++ public/src/components/Home/index.jsx
@@ -7,8 +7,8 @@ import './home.scss';
 // assets
 import ramen from '../../../dist/assets/works/tako_main.jpg';
 import fd from '../../../dist/assets/works/fds_banner.jpg';
-import nike from '../../../dist/assets/works/ride.jpg'
-import salonshoot from '../../../dist/assets/works/DSC00602.jpg';
+import nike from '../../../dist/master/yose.jpg'
+import salonshoot from '../../../dist/assets/works/DSC00602wide.jpg';
 import reel from '../../../dist/assets/photography/DSC01329-Enhanced.jpg';
 
 let works = [
@@ -16,45 +16,35 @@ let works = [
     id: 1,
     title: "Tako Poke + Ramen",
     image: ramen,
-    description: "We developed a modern website that showcased Tako Poke + Ramen's values through story telling highlighted by its visual aesthetic. In order to achieve this, we focused on capturing vibrant imagery that highlighted the final food product, as well as taking customers on a visceral journey in food preparation. By focusing on the food and its creation process, we brought the dine-in experience into the customers’ homes that would not otherwise exist.",
     link: "/takopokeramen",
     isActive: false,
-    color: "#BEB082",
-    position: "flex-start",
-    img_position: "45%"
+    description: "branding, photography, web"
   },
   {
     id: 2,
-    title: "Riders Co",
-    image: nike,
-    description: "Our goal is to create a seamless experience for users, from casual outdoor enthusiasts to seasoned veterans, a platform that will provide quick, user aggregated and curated destinations while integrating fitness tracking solutions that we all come to appreciate.",
-    link: "/ride",
-    isActive: true,
-    color: "#82A7BE",
-    position: "flex-end",
-    img_position: "-10%"
-  },
-  // {
-  //   id: 3,
-  //   title: "Oribe Hair Competition",
-  //   image: salonshoot,
-  //   // description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam",
-  //   link: "",
-  //   isActive: false,
-  //   color: "#BEB082"
-
-  // },
-  {
-    id: 4,
     title: "Fashion Domino",
     image: fd,
-    description: "Fashion Domino is the premium Los Angeles based Wholesale Fashion Marketplace that connects wholesale clothing, accessories, handbags, shoes and cosmetics manufacturers & distributors with buyers from across the USA and the whole globe.",
     link: "/fashiondomino",
     isActive: false,
-    color: "#BE8290",
-    position: "flex-start",
-    img_position: "45%"
+    description: "graphic design, strategy"
   },
+  {
+    id: 3,
+    title: "Riders Co",
+    image: nike,
+    link: "/ride",
+    isActive: true,
+    description: "branding, design, web"
+  },
+  {
+    id: 4,
+    title: "Oribe Hair Competition",
+    image: salonshoot,
+    link: "/one2one",
+    isActive: false,
+    description: "photography"
+  },
+
 ]
 
 const Landing = () => {
@@ -113,32 +103,20 @@ const Landing = () => {
       </section>
       <section className="landing__works">
         <h2 className="header">Selected Works</h2>
-        <div className="landing__works__container">
-          {/* <div className="landing__works__main" >
+        {/* <div className="landing__works__container"> */}
+          <div className="landing__works__main" >
             { workList.map((work => {
-                return <div className="landing__work" style={{background: `${work.color}`}}>
-                <img className="shadow" src={work.image}/>
-                <h3>{work.title}</h3>
-                <p>{work.description}</p>
-                <Link className="roundbutton black space" to={work.link}>View Project</Link>
+                return <div className="landing__work">
+                <Link to={work.link} className="image__zoom">
+                  <img src={work.image}/>
+                </Link>
+                <div className="landing__text">
+                  <h3>{work.title}</h3>
+                  <p>{work.description}</p>
+                </div>
             </div>
             }))}
-          </div> */}
-          {/* {workList.map((work => {
-            return <div className={`landing__works__main ${work.isActive ? "active" : ""}`}>
-              <motion.img className={`works__hero ${work.isActive ? "hero-full" : "hero-shrunk"}`} src={work.image} alt="" onClick={() => changeView(work)}
-                animate={{width: width + "320px"}}
-                // onClick={() => changeView(width)}
-              />
-              <div className="landing__works__copy">
-                <h3>{work.title}</h3>
-                <p>{work.description}</p>
-                <Link className="roundbutton black space" to={work.link}>View Project</Link>
-              </div>
-            </div>
-          }))} */}
-
-        </div>
+          </div>
         <Link className="roundbutton black space" to="/works">
           See All
         </Link>
